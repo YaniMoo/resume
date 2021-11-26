ec69269e68a1d93582bae9fd9b28c3b489d1ae9c
yani_dosetech <yani.d@dosetech.co>
2021-11-26 16:13:46 +0700

    update th

--- pages/index.vue
+++ pages/index.vue
@@ -12,18 +12,85 @@
     </b-row>
     <b-row>
       <b-col md="4" class="py-3">
-        <p>ข้อมูลส่วนตัว</p>
-        <p>เพศ: <span>หญิง</span></p>
-        
-        วันเกิด:  18 มีนาคม 2538
-        อายุ: 26 ปี
-        สูง: 158 ซม.
-        หนัก: 48 กก.
-        สถานภาพสมรส: โสด
-        ศาสนา: พุทธ
+        <div>
+          <p class="txt-title">ข้อมูลส่วนตัว</p>
+          <p class="txt-sub-title">เพศ: <span class="txt-detail">หญิง</span></p>
+          <p class="txt-sub-title">วันเกิด: <span class="txt-detail">18 มีนาคม 2538</span></p>
+          <p class="txt-sub-title">อายุ: <span class="txt-detail">26 ปี</span></p>
+          <p class="txt-sub-title">สูง: <span class="txt-detail">158 ซม.</span></p>
+          <p class="txt-sub-title">หนัก: <span class="txt-detail">48 กก.</span></p>
+          <p class="txt-sub-title">สถานภาพสมรส: <span class="txt-detail">โสด</span></p>
+          <p class="txt-sub-title">ศาสนา: <span class="txt-detail">พุทธ</span></p>
+        </div>
+        <div>
+          <p class="txt-title">ติดต่อ</p>
+          <p class="txt-sub-title">เบอร์โทรศัพท์: <span class="txt-detail">083706XXXX</span></p>
+          <p class="txt-sub-title">อีเมล์: <span class="txt-detail">yanee_muay_2011@hotmail.com</span></p>
+          <p class="txt-sub-title">ที่อยู่: <span class="txt-detail">บัดเจท คอนโด ซ.ติวานนท์ 3 ต.ตลาดขวัญ อ.เมืองนนทบุรี จ.นนทบุรี 11000</span></p>
+        </div>
       </b-col>
       <b-col md="8" class="py-3">
+        <p class="txt-title">ประวัติการทำงาน</p>
+        <div>
+          <p class="txt-sub-title">Senior Front-End Developer</p>
+          <p class="txt-detail font-weight-bold">บริษัท โดสเทค จำกัด | พฤศจิกายน 2561 - ปัจจุบัน</p>
+          <ul class="txt-detail">
+            <li>พัฒนาโปรแกรมโดยใช้ frameworks Nuxt.js, Vue.js, .Net Core </li>
+            <li>พัฒนาซอฟต์แวร์ตามความต้องการของลูกค้า</li>
+            <li>พัฒนาเว็บไซต์โดยใช้ frameworks ที่ทันสมัย</li>
+            <li>ตรวจสอบความเป็นไปได้ของซอฟต์แวร์</li>
+            <li>ปรับปรุงโค้ดและแก้ไขข้อผิดพลาดของซอฟต์แวร์</li>
+          </ul>
+        </div>
+
+         <div>
+          <p class="txt-sub-title">PHP Programmer</p>
+          <p class="txt-detail font-weight-bold">บริษัท โกลบอล ไวร์เลส จำกัด | พฤษภาคม 2560 – สิงหาคม 2561</p>
+          <ul class="txt-detail">
+            <li>พัฒนาโปรแกรมโดยใช้ Laravel framework</li>
+            <li>พัฒนาซอฟต์แวร์ตามความต้องการของลูกค้า</li>
+            <li>ตรวจสอบความเป็นไปได้ของซอฟต์แวร์</li>
+            <li>ปรับปรุงโค้ดและแก้ไขข้อผิดพลาดของซอฟต์แวร์</li>
+          </ul>
+        </div>
+
+        <p class="txt-title">ประวัติการทำงาน</p>
+        <div>
+          <p class="txt-sub-title">มหาวิทยาลัยราชภัฏนครปฐม จังหวัดนครปฐม</p>
+          <p class="txt-detail font-weight-bold">ระดับการศึกษา ปริญญาตรี คณะ วิทยาศาสตร์และเทคโนโลยี <br /> สาขา วิศวกรรมซอฟต์แวร์ | 2556 - 2560</p>
+          <p class="txt-detail">เกรดเฉลี่ย 3.77 (เกียรตินิยมอันดับ 1)</p>
+        </div>
+        <div>
+          <p class="txt-sub-title">วิทยาลัยการอาชีพสองพี่น้อง จังหวัดสุพรรณบุรี</p>
+          <p class="txt-detail font-weight-bold">ระดับการศึกษา ประกาศนียบัตรวิชาชีพ (ปวช.) สาขาวิชา พณิชยการ <br/> สาขางาน คอมพิวเตอร์ธุรกิจ | 2553 – 2556</p>
+          <p class="txt-detail">เกรดเฉลี่ย 3.78</p>
+        </div>
+
+        <p class="txt-title">ประสบการ์อื่นๆ</p>
+        <div>
+          <p class="txt-sub-title">เข้าร่วมในการแข่งขัน การแข่งขันการเขียนโปรแกรมคอมพิวเตอร์ ACM-ICPC 2015</p>
+        </div>
         
+        <p class="txt-title">ทักษะ/ความสามารถ</p>
+        <div>
+          <p class="txt-sub-title">Coding Languages</p>
+          <ul class="txt-detail">
+            <li>HTML/HTML5, CSS/CSS3</li>
+            <li>JavaScript, PHP</li>
+            <li>MySQL</li>
+          </ul>
+        </div>
+
+        <div>
+          <p class="txt-sub-title">Frameworks/Systems</p>
+          <ul class="txt-detail">
+            <li>Nuxt.js, Vue.js</li>
+            <li>Bootstrap 3, 4</li>
+            <li>Laravel</li>
+            <li>JQuery, Axios, Ajax</li>
+          </ul>
+        </div>
+
       </b-col>
     </b-row>
   </b-container>
